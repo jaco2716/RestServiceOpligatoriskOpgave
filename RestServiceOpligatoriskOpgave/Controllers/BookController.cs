15363af5048e705057161dce343f5bce66d71898
jaco2716 <jacob.welin@hotmail.com>
2019-10-02 13:13:33 +0200

    Get put post delete

--- RestServiceOpligatoriskOpgave/Controllers/BookController.cs
+++ RestServiceOpligatoriskOpgave/Controllers/BookController.cs
@@ -12,7 +12,7 @@ namespace RestServiceOpligatoriskOpgave.Controllers
     [ApiController]
     public class BookController : ControllerBase
     {
-        private static readonly List<Book> Books = new List<Book>()
+        private static readonly List<Book> books = new List<Book>()
         {
             new Book("Harry Potter", "Gertrud", 560, "1234567890123"),
             new Book("Putta Horri", "Felix", 350, "3210987654321"),
@@ -24,34 +24,46 @@ namespace RestServiceOpligatoriskOpgave.Controllers
 
         // GET: api/Book
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Book> Get()
         {
-            return new string[] { "value1", "value2" };
+            return books;
         }
 
         // GET: api/Book/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Book Get(string isbn13)
         {
-            return "value";
+            return books.Find(i => i.Isbn13 == isbn13);
         }
 
         // POST: api/Book
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Book value)
         {
+            books.Add(value);
         }
 
         // PUT: api/Book/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(string isbn13, [FromBody] Book value)
         {
+            Book book = Get(isbn13);
+            if (book != null)
+            {
+                book.Title = value.Title;
+                book.Auther = value.Auther;
+                book.PageNo = value.PageNo;
+                book.Isbn13 = value.Isbn13;
+            }
+
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(string isbn13)
         {
+            Book book = Get(isbn13);
+            books.Remove(book);
         }
     }
 }
